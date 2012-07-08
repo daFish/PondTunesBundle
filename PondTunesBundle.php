01ca066c70f2da517aed336aaa0c02a08220cb2c
daFish <dafish@soundtrack-board.de>
2012-07-08 17:12:25 +0200

    Fixed bundle class issue with namespace.

--- PondTunesBundle.php
+++ PondTunesBundle.php
@@ -9,7 +9,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Pond\Tunes;
+namespace Pond\TunesBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
